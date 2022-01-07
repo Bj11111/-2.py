e5059017f6e3f77df69e0a87c06963a2dc224e8f
Bj11111 <95857589+Bj11111@users.noreply.github.com>
2022-01-07 11:55:17 +0800

    Update 2.py

--- 2.py
+++ 2.py
@@ -26,7 +26,8 @@ def print_board():
     st.write(dict[1], '|' , dict[2] , '|', dict[3])
      
 def main( ):
-    move1 = int(text_input('你想走哪一格?'))
+#     move1 = int(text_input('你想走哪一格?'))
+      move1 = st.slider('你想走哪一格?'))
     if dict[move1] == ' ':
       dict[move1] = 'X'
     else:
