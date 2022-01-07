7bd5531b9f72925c0d79cf7b2cca9caaace55abd
Bj11111 <95857589+Bj11111@users.noreply.github.com>
2022-01-07 12:03:13 +0800

    Update 2.py

--- 2.py
+++ 2.py
@@ -32,7 +32,7 @@ def main( ):
         dict[move1] = 'X'
       else:
           st.write('這是無效移動')
-    print_board()
+      print_board()
 
     move2 = int(text_input('你想走哪一格?'))
     if dict[move2] == ' ':
