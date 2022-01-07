0a042d6121fb52a1607da10d28c910f275869983
Bj11111 <95857589+Bj11111@users.noreply.github.com>
2022-01-07 08:29:53 +0800

    Update 2.py

--- 2.py
+++ 2.py
@@ -1,94 +1,48 @@
-import turtle
-import random
+dict = {}
+for i in range(1,10):
+    dict[i] = ' '
 
-my_wn = turtle.Screen()
-my_pen = turtle.Turtle()	#my turtle
-my_pen.speed(0)
-my_wn.bgcolor("grey")
-my_pen.color("white", "light blue")
-my_wn.tracer(1, 10)
+def check():
+    if (dict[1] == dict[2]== dict[3] != ' ' 
+    or dict[4] == dict[5]== dict[6] != ' '
+    or dict[7] == dict[8]== dict[9] != ' '
+    or dict[1] == dict[4]== dict[7] != ' '
+    or dict[2] == dict[5]== dict[8] != ' '
+    or dict[3] == dict[6]== dict[9] != ' '
+    or dict[1] == dict[5]== dict[9] != ' '
+    or dict[3] == dict[5]== dict[7] != ' ' ):
+        return True
 
+def print_board():
+    print(dict[7], '|' , dict[8] , '|', dict[9])
+    print('- + - + -') 
+    print(dict[4], '|' , dict[5] , '|', dict[6])
+    print('- + - + -')
+    print(dict[1], '|' , dict[2] , '|', dict[3])
+     
+def main( ):
+    move1 = int(input('你想怎麼走?'))
+    if dict[move1] == ' ':
+      dict[move1] = 'X'
+    else:
+        print('這是被禁止的')
+    print_board()
 
-class Game():
-	g1x = 50	#start pos
-	g1y = 50
-	g2x = -50	#start pos
-	g2y = -50
-	
-	dx = 0	#wektor przesuniecia
-	dy = 10
-	
-	speed = 200
-	taken = []	#zajete miejsca
+    move2 = int(input('你想怎麼走?'))
+    if dict[move2] == ' ':
+        dict[move2] = 'O'
+    else:
+        print('這是被禁止的')
+    print_board()
+    
+    if check():
+        print('game over')
+    else:
+        main()
 
-game = Game()
+a = input('想玩井字遊戲嗎？（是/否): ')
+if a =='是':
 
-
-
-def teleport():		#leci na miejsce
-	my_pen.up()
-	my_pen.goto(game.g1x, game.g1y)
-	my_pen.down()
-
-def my_square(a):	#rysuje kwadrat
-	teleport()
-	my_pen.begin_fill()
-	for i in range(4):
-		my_pen.forward(10)
-		my_pen.right(90)
-	my_pen.end_fill()
-	
-	game.taken.append((game.g1x,game.g1y))	#dodanie ruchu do woreczka
-	#print(game.taken)
-	
-def move_up():
-	game.dy = 10
-	game.dx = 0
-	
-	
-def move_down():
-	game.dy = -10
-	game.dx = 0
-	
-
-def move_left():
-	game.dx = -10
-	game.dy = 0
-	
-
-def move_right():
-	game.dx = 10
-	game.dy = 0
-	
-
-def game_loop():
-	game.g1x += game.dx
-	game.g1y += game.dy
-	my_square(10)
-
-	
-	my_wn.ontimer(game_loop, game.speed)
-
-
-#gra
-teleport()
-my_square(10)
-game_loop()
-
-my_wn.listen()
-my_wn.onkeypress(move_up, "Up")
-my_wn.onkeypress(move_down, "Down")
-my_wn.onkeypress(move_left, "Left")
-my_wn.onkeypress(move_right, "Right")
-
-my_wn.delay(0)
-
-
-turtle.done()
-# BIBLIOTEKI TURTLOWE
-#def done():
-#	while True:
-#		if nacisnieto_klawisz():
-#			uruchom_onkeypress_zarejestrowany();
-#		if czas_minal():
-#			uruchom_timery()
+    main()
+else:
+    print('可憐')
