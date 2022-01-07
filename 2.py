d5f668574b9ce13425414e8bfb66da86bd85160f
Bj11111 <95857589+Bj11111@users.noreply.github.com>
2022-01-07 12:00:44 +0800

    Update 2.py

--- 2.py
+++ 2.py
@@ -28,7 +28,7 @@ def print_board():
 def main( ):
 #     move1 = int(text_input('你想走哪一格?'))
       move1 =  st.number_input('你想走哪一格?')
-    if dict[move1] == ' ':
+    if dict[move1] == ' ' :
         dict[move1] = 'X'
     else:
         st.write('這是無效移動')
