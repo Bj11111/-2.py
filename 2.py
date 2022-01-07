15756f28058cf3ad68ae2b379cd199dda46396ee
Bj11111 <95857589+Bj11111@users.noreply.github.com>
2022-01-07 11:56:44 +0800

    Update 2.py

--- 2.py
+++ 2.py
@@ -29,7 +29,7 @@ def main( ):
 #     move1 = int(text_input('你想走哪一格?'))
       move1 = st.slider('你想走哪一格?')
     if dict[move1] == ' ':
-      dict[move1] = 'X'
+        dict[move1] = 'X'
     else:
         print('這是無效移動')
     print_board()
